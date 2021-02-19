162158560a35fa21019be3b62b9986f8530d3732
Dayal ND <dave_eqt@yahoo.com>
2021-02-19 13:16:22 +0530

    Create CollectionViewCell.swift

---
+++ FoodApp/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  FoodApp
+//
+//  Created by Dayal N D on 17/02/21.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
+    
+}
